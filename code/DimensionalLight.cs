a0bba00ebc8284ebe38229d57e13c21a7f961b13
MuffinTastic <muffintastic32@gmail.com>
2022-11-07 18:02:29 -0800

    populate DimensionalLight, use it

--- code/DimensionalLight.cs
+++ code/DimensionalLight.cs
@@ -3,12 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sandbox;
 
 namespace SpaceTest;
-internal class DimensionalLight : DimensionalEntity
+public partial class DimensionalLight : DimensionalEntity
 {
+	[Net, Change] public new Vector3 Position { get; set; }
+	[Net, Change] public float Radius { get; set; }
+	[Net, Change] public float LinearAttenuation { get; set; }
+	[Net, Change] public float QuadraticAttenuation { get; set; }
+	[Net, Change] public Color ColorTint { get; set; }
+
+	private SceneLight _sceneLight;
+
 	public DimensionalLight()
 	{
 
+	}
+
+	public DimensionalLight( Dimension dimension ) : base( dimension )
+	{
+
+	}
+
+	public override void InitSceneObject()
+	{
+		_sceneLight = new SceneLight( Dimension.SceneWorld );
+		_sceneLight.Position = Position;
+		_sceneLight.Radius = Radius;
+		_sceneLight.LinearAttenuation = LinearAttenuation;
+		_sceneLight.QuadraticAttenuation = QuadraticAttenuation;
+		_sceneLight.ColorTint = ColorTint;
+	}
+
+	public void OnPositionChanged( Vector3 _, Vector3 newPosition )
+	{
+		if ( _sceneLight is null ) return;
+		_sceneLight.Position = newPosition;
+	}
+
+	public void OnRadiusChanged( float _, float newRadius )
+	{
+		if ( _sceneLight is null ) return;
+		_sceneLight.Radius = newRadius;
+	}
+
+	public void OnLinearAttenuationChanged( float _, float newLinearAttenuation )
+	{
+		if ( _sceneLight is null ) return;
+		_sceneLight.LinearAttenuation = newLinearAttenuation;
+	}
+
+	public void OnQuadraticAttenuationChanged( float _, float newQuadraticAttenuation )
+	{
+		if ( _sceneLight is null ) return;
+		_sceneLight.QuadraticAttenuation = newQuadraticAttenuation;
+	}
+
+	public void OnColorTintChanged( Color _, Color newColorTint )
+	{
+		if ( _sceneLight is null ) return;
+		_sceneLight.ColorTint = newColorTint;
 	}
 }
--- code/Game.cs
+++ code/Game.cs
@@ -34,6 +34,8 @@ public partial class SpaceGame : Sandbox.Game
 			await Util.AwaitCondition( () => DimensionManager is not null );
 			await DimensionManager.WaitForDimensions();
 
+
+
 			var box1 = new DimensionalModelEntity( dimension1 );
 			box1.SetModel( "models/citizen_props/crate01.vmdl" );
 			box1.Position = new Vector3( 100.0f, 50.0f, 0.0f );
@@ -48,6 +50,30 @@ public partial class SpaceGame : Sandbox.Game
 			box3.SetModel( "models/citizen_props/crate01.vmdl" );
 			box3.Position = new Vector3( 150.0f, -50.0f, 0.0f );
 			box3.Rotation = Rotation.Random;
+
+
+
+			var light = new DimensionalLight( dimension1 );
+			light.Position = Vector3.One * 50.0f;
+			light.Radius = 500.0f;
+			light.LinearAttenuation = 15.0f;
+			light.QuadraticAttenuation = 0.0f;
+
+			var light2 = new DimensionalLight( dimension2 );
+			light2.Position = new Vector3( -250.0f, 50.0f, -150.0f );
+			light2.Radius = 1500.0f;
+			light2.LinearAttenuation = 20.0f;
+			light2.QuadraticAttenuation = 0.0f;
+
+			var light3 = new DimensionalLight( dimension2 );
+			//light3.Position = Vector3.Right * 50.0f + Vector3.Forward * 25.0f + Vector3.Down * 30.0f;
+			light3.Position = new Vector3( -440.0f, 600.0f, 96.0f );
+			light3.Radius = 500.0f;
+			light3.LinearAttenuation = 25.0f;
+			light3.QuadraticAttenuation = 0.0f;
+			light3.ColorTint = Color.Orange;
+
+
 
 			var portal = DimensionManager.NewPortal( dimension1, dimension2 ); // new DimensionalPortal( dimension1, dimension2 );
 			portal.Rotation = Rotation.FromYaw( -45.0f );
@@ -84,10 +110,6 @@ public partial class SpaceGame : Sandbox.Game
 			//citizen.Position = portal.Rotation.Backward * 750.0f;
 			//citizen.Rotation = Rotation.LookAt( -citizen.Position.Normal, Vector3.Up );
 			//Dimension2.AddAndInitEntity( citizen );
-
-			var light = new PointLightEntity();
-			light.Position = Vector3.Zero;
-			light.Brightness = 0.25f;
 		}
 	}
 
@@ -96,53 +118,21 @@ public partial class SpaceGame : Sandbox.Game
 	Vector3 rotAxis = new Vector3( 1.0f, 2.0f, 3.0f).Normal;
 	Vector3 originalPos = new Vector3( -300.0f, -500.0f, 0.0f );
 
-	public override async void ClientSpawn()
+	public override void ClientSpawn()
 	{
 		Map.Camera.AddHook( new DimensionBlitHook() );
-
-		await DimensionManager.WaitForDimensions();
-
-		/*
-		var light = new SceneLight( Dimension1.SceneWorld );
-		light.Position = Vector3.One * 50.0f;
-		light.Radius = 500.0f;
-		light.LinearAttenuation = 15.0f;
-		light.QuadraticAttenuation = 0.0f;
-
-		var light2 = new SceneLight( Dimension2.SceneWorld );
-		light2.Position = new Vector3( -250.0f, 50.0f, -150.0f );
-		light2.Radius = 1500.0f;
-		light2.LinearAttenuation = 20.0f;
-		light2.QuadraticAttenuation = 0.0f;
-
-		var light3 = new SceneLight( Dimension2.SceneWorld );
-		//light3.Position = Vector3.Right * 50.0f + Vector3.Forward * 25.0f + Vector3.Down * 30.0f;
-		light3.Position = new Vector3( -440.0f, 600.0f, 96.0f );
-		light3.Radius = 500.0f;
-		light3.LinearAttenuation = 25.0f;
-		light3.QuadraticAttenuation = 0.0f;
-		light3.ColorTint = Color.Orange;
-		*/
-
-		//DebugOverlay.Sphere( light3.Position, light3.Radius / 10.0f, Color.Orange, 30.0f, depthTest: false );
 	}
 
 	[Event.Tick.Server]
-	public void Swappy()
+	public void UpdateRotatyPortal()
 	{
-		/*foreach ( var ent in Entity.All.OfType<DimensionalModelEntity>() )
-		{
-			DebugOverlay.Sphere( ent.Position, 0.5f, ent.Dimension == Dimension1 ? Color.Red : Color.Cyan, depthTest: false );
-		}*/
-
 		rotAxis *= Rotation.RotateAroundAxis( Vector3.Up, Time.Delta * 30.0f );
-		//RotatyPortal.Rotation *= Rotation.RotateAroundAxis( rotAxis, Time.Delta * 30.0f );
+		RotatyPortal.Rotation *= Rotation.RotateAroundAxis( rotAxis, Time.Delta * 30.0f );
 
 		//var offset = new Vector3( MathF.Sin( Time.Now / 3.5f ) * 250.0f, MathF.Cos( Time.Now / 3.5f ) * 250.0f, MathF.Sin( Time.Now / 4.5f ) * 75.0f );
 		var offset = new Vector3( MathF.Sin( Time.Now / 3.5f ) * 250.0f, MathF.Cos( Time.Now / 3.5f ) * 250.0f, 0.0f );
 
-		RotatyPortal.Rotation = Rotation.LookAt( offset.Normal, Vector3.Up ) * Rotation.FromYaw( 90.0f );
-
+		//RotatyPortal.Rotation = Rotation.LookAt( offset.Normal, Vector3.Up ) * Rotation.FromYaw( 90.0f );
 		RotatyPortal.Position = originalPos + offset;
 	}
 
