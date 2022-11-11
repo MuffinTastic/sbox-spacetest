d2e803e1c98e764933190db842c96a05c7dba696
MuffinTastic <muffintastic32@gmail.com>
2022-11-11 04:49:43 -0800

    remove dead code

--- code/DimensionBlitHook.cs
+++ code/DimensionBlitHook.cs
@@ -41,7 +41,6 @@ public class DimensionBlitHook : RenderHook
 
 			if ( dimension is not null )
 			{
-				//RenderAllPortals( target, dimension );
 				Graphics.RenderToTexture( dimension.SceneCamera, renderTarget.ColorTarget );
 			}
 
@@ -53,92 +52,5 @@ public class DimensionBlitHook : RenderHook
 
 			Graphics.Blit( ScreenBlit, attributes );
 		}
-	}
-
-	public void RenderAllPortals( SceneCamera target, Dimension mainViewDimension )
-	{
-		var connectedPortals = Entity.All.OfType<DimensionalPortal>()
-			.Where( p => p.IsConnected( mainViewDimension ) )
-			.OrderBy( p => p.Position.Distance( mainViewDimension.SceneCamera.Position ) );
-
-		CurrentPortal = 0;
-
-		foreach ( var portal in connectedPortals )
-		{
-			//var sceneObject = portal.GetScenePortalForDimension( mainViewDimension );
-			//RenderPortal( target, mainViewDimension, portal );
-			//Graphics.Render( sceneObject );
-
-			if ( CurrentPortal >= 8 ) break;
-		}
-	}
-
-	public static int CurrentPortal = 0;
-
-	private void RenderPortal( SceneCamera target, Dimension dimension, DimensionalPortal portal )
-	{
-		if ( portal.DoneRendering )
-			return;
-
-		portal.SetDoneRendering();
-
-		// TODO: Floating origin
-
-		var portalPlane = new Plane(
-			portal.Position,
-			portal.Rotation.Forward
-		);
-
-		var portalRotation = Rotation.LookAt( portalPlane.Normal, Vector3.Up );
-
-		if ( portal.VertexPositions is null ) return;
-		var vertices = portal.VertexPositions.Select( v => new Vertex( portal.Position + v, Vector4.Zero, Color32.White ) ).ToArray();
-
-		var relative = target.Position - portal.Position;
-		var dot = relative.Dot( portalPlane.Normal );
-
-		Vector4 clip = new Vector4( portalPlane.Normal, portalPlane.Distance );
-		bool flip = portal.IsFirstDimension( dimension );
-		float flipdot = flip ? dot : -dot;
-		bool front = flipdot > 0.0f;
-
-		if ( !front )
-		{
-			return;
-		}
-
-		if ( flip )
-		{
-			clip = -clip;
-		}
-
-		//DebugOverlay.Sphere( Vector3.Zero, 5.0f, Color.Red, depthTest: false );
-		//DebugOverlay.Line( portalPlane.Normal * portalPlane.Distance, portalPlane.Normal * portalPlane.Distance + portalPlane.Normal * 100.0f, Color.Cyan, depthTest: false );
-
-		var otherDimension = portal.GetOtherDimension( dimension );
-		var camera = otherDimension.SceneCamera;
-
-		camera.Attributes.SetCombo( "D_ENABLE_USER_CLIP_PLANE", true );
-		camera.Attributes.Set( "EnableClipPlane", true );
-		camera.Attributes.Set( "ClipPlane0", clip );
-
-		using var renderTarget = RenderTarget.GetTemporary();
-
-		Graphics.Clear( Color.Black, false, true );
-
-		Graphics.RenderToTexture( camera, renderTarget.ColorTarget );
-
-		camera.Attributes.Clear();
-
-		var materialAttributes = new RenderAttributes();
-		target.Attributes.Clear();
-		target.Attributes.SetCombo( "D_PORTAL_ID", CurrentPortal );
-		target.Attributes.Set( $"ColorBuffer{CurrentPortal}", renderTarget.ColorTarget );
-		Graphics.Draw( vertices, vertices.Length, PortalBlit, materialAttributes );
-
-		var flipstring = flip ? "First" : "Second";
-		Log.Info( $"{flipstring} ColorBuffer{CurrentPortal} {portal} {otherDimension}" );
-
-		CurrentPortal++;
 	}
 }
